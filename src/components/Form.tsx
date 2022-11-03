ef92b38502dafaee552f364f52e91ce687d7074a
cleisonmp <cleison.mp@gmail.com>
2022-11-03 17:00:56 -0300

    init :sparkles

---
+++ src/components/Form.tsx
@@ -0,0 +1,58 @@
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { trpc } from "../utils/trpc";
+
+export const Form = () => {
+  const { data: session, status } = useSession();
+  const [message, setMessage] = useState("");
+  const utils = trpc.useContext();
+  const postMessage = trpc.guestbook.postMessage.useMutation({
+    onMutate: () => {
+      utils.guestbook.getAll.cancel();
+      const optimisticUpdate = utils.guestbook.getAll.getData();
+
+      if (optimisticUpdate) {
+        utils.guestbook.getAll.setData(optimisticUpdate);
+      }
+    },
+    onSettled: () => {
+      utils.guestbook.getAll.invalidate();
+    },
+  });
+
+  if (!session) {
+    return null;
+  }
+
+  return (
+    <form
+      className="flex gap-2"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        postMessage.mutate({
+          name: session.user?.name as string,
+          message,
+        });
+
+        setMessage("");
+      }}
+    >
+      <input
+        type="text"
+        value={message}
+        placeholder="Your message..."
+        minLength={2}
+        maxLength={100}
+        onChange={(event) => setMessage(event.target.value)}
+        className="rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
+      />
+      <button
+        type="submit"
+        className="rounded-md border-2 border-zinc-800 p-2 focus:outline-none"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
---
+++ src/components/Messages.tsx
@@ -0,0 +1,20 @@
+import { trpc } from "../utils/trpc";
+
+export const Messages = () => {
+  const { data: messages, isLoading } = trpc.guestbook.getAll.useQuery();
+
+  if (isLoading) return <div>Fetching messages...</div>;
+
+  return (
+    <div className="flex flex-col gap-4">
+      {messages?.map((msg, index) => {
+        return (
+          <div key={index}>
+            <p>{msg.message}</p>
+            <span>- {msg.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};

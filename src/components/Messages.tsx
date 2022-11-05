3d6416d04f9dcff528a8b9d8b635e4988b304e3e
cleisonmp <cleison.mp@gmail.com>
2022-11-05 16:56:33 -0300

    styles: change page styles

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,4 +1,5 @@
 import { trpc } from "../utils/trpc";
+import { Loading } from "./common/loading";
 
 export const Messages = () => {
   const {
@@ -10,20 +11,28 @@ export const Messages = () => {
     refetchOnWindowFocus: false,
   });
 
-  if (isLoading) return <div>Fetching messages...</div>;
-
   return (
-    <div className="flex flex-col gap-4 border p-4">
-      {messages?.map((msg, index) => {
-        return (
-          <div key={index}>
-            {isFetching && <p>Updating...</p>}
-
-            <p>{msg.message}</p>
-            <span className="text-gray-500">From: {msg.name}</span>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="relative flex items-center justify-center gap-1">
+        <div className="absolute left-0">
+          {isFetching && <Loading width="20" height="20" />}
+        </div>
+        <p className="text-center ">Message board</p>
+      </div>
+      {isLoading ? (
+        <div>Fetching messages...</div>
+      ) : (
+        <div className="mt-2 flex h-96 w-full flex-col gap-4 overflow-auto border p-4">
+          {messages?.map((msg, index) => {
+            return (
+              <div key={index} className="w-full">
+                <p className="">{msg.message}</p>
+                <span className="text-gray-500">From: {msg.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Link from "next/link";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,6 +12,23 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <header className="sticky top-0 z-50 bg-neutral-900">
+        <p>Pages:</p>
+        <nav className="flex gap-4 p-4">
+          <Link href="/" className="underline">
+            Home
+          </Link>
+          <Link href="image" className="underline">
+            Image Upload
+          </Link>
+          <Link href="secret" className="underline">
+            Private
+          </Link>
+          <Link href="notsecret" className="underline">
+            Public
+          </Link>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </SessionProvider>
   );
